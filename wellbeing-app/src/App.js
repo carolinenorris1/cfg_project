4ced5e1e765cde6bc1ef5c7a1fa6f2c8e0ab7352
Caroline Norris <caroline.norris91@gmail.com>
2023-08-05 15:31:08 +0100

    Updated App.js for our team member profiles

--- wellbeing-app/src/App.js
+++ wellbeing-app/src/App.js
@@ -5,18 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>CFG Group Project - Team 4</h1>
+        <h2>Team Member Profiles</h2>
       </header>
     </div>
   );
