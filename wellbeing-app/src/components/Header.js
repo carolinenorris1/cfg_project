28a521662d3e61d6925da15542901274d22556cd
NullishKoala <125359361+NullishKoala@users.noreply.github.com>
2023-08-22 23:40:02 +0200

    fix modal styling

--- wellbeing-app/src/components/Header.js
+++ wellbeing-app/src/components/Header.js
@@ -5,7 +5,6 @@ import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Modal from "react-bootstrap/Modal";
 
-
 function Header({ setSelectedPage }) {
   const context = useContext(CountryContext);
   const country = context.country;
@@ -22,57 +21,65 @@ function Header({ setSelectedPage }) {
   };
 
   return (
-    <div className={"header-container"}>
-      <div className={"logo"}>
-        <h1>LOGO</h1>
-        {/* place for the logo - image */}
-      </div>
+    <div>
+      <div className={"header-container"}>
+        <div className={"logo"}>
+          <h1>LOGO</h1>
+          {/* place for the logo - image */}
+        </div>
 
-      <div className={"header-menu"}>
-        <div></div>
-        <button onClick={() => setSelectedPage("home")}>Home</button>
-        <button onClick={() => setSelectedPage("wellnessApp")}>
-          WellnessApp
-        </button>
-        <button onClick={() => setSelectedPage("wellbeing")}>Wellbeing</button>
-        <button onClick={() => setSelectedPage("community")}>Community</button>
-      </div>
-      <div>{/* image for user */}</div>
-      <div>
-        <Button variant="primary" onClick={handleShow}>
-          {country}
-        </Button>
+        <div className={"header-menu"}>
+          <div></div>
+          <button onClick={() => setSelectedPage("home")}>Home</button>
+          <button onClick={() => setSelectedPage("wellnessApp")}>
+            WellnessApp
+          </button>
+          <button onClick={() => setSelectedPage("wellbeing")}>
+            Wellbeing
+          </button>
+          <button onClick={() => setSelectedPage("community")}>
+            Community
+          </button>
+        </div>
+        <div className={"user-icon-container"}>
+          <img
+            src="images\home-images\vectoruser-icon.png"
+            className={"user-icon"}
+          ></img>
+        </div>
 
-        <Modal show={show} onHide={handleClose}>
-          <Modal.Header closeButton>
-            <Modal.Title>
-              Where in the world are you visiting us from?
-            </Modal.Title>
-          </Modal.Header>
-          <Form onSubmit={handleSubmit}>
-            <Modal.Body>
-              <Form.Select name="country">
-                <option value="UK">UK</option>
-                <option value="US">US</option>
-                <option value="EU">EU</option>
-              </Form.Select>
-            </Modal.Body>
-            <Modal.Footer>
-              Can't see your region listed? We hope to be available to you soon!
-              <Button variant="secondary" onClick={handleClose}>
-                Close
-              </Button>
-              <Button variant="primary" type="submit">
-                Update
-              </Button>
-            </Modal.Footer>
-          </Form>
-        </Modal>
-      <div className={"user-icon-container"}>
-        <img
-          src="images\home-images\vectoruser-icon.png"
-          className={"user-icon"}
-        ></img>
+        <div className={"country-modal"}>
+          <Button variant="primary" onClick={handleShow}>
+            {country}
+          </Button>
+
+          <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+              <Modal.Title>
+                Where in the world are you visiting us from?
+              </Modal.Title>
+            </Modal.Header>
+            <Form onSubmit={handleSubmit}>
+              <Modal.Body>
+                <Form.Select name="country">
+                  <option value="UK">UK</option>
+                  <option value="US">US</option>
+                  <option value="EU">EU</option>
+                </Form.Select>
+              </Modal.Body>
+              <Modal.Footer>
+                Can't see your region listed? We hope to be available to you
+                soon!
+                <Button variant="secondary" onClick={handleClose}>
+                  Close
+                </Button>
+                <Button variant="primary" type="submit">
+                  Update
+                </Button>
+              </Modal.Footer>
+            </Form>
+          </Modal>
+        </div>
       </div>
     </div>
   );
